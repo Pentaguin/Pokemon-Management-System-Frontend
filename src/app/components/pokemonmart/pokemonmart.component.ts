034615a96da19d3fcbe74392a0d4bf2af5e4d2dd
Pentaguin <davidtai171732@gmail.com>
2024-11-29 12:29:58 +0100

    Added popup menu for shop

--- src/app/components/pokemonmart/pokemonmart.component.ts
+++ src/app/components/pokemonmart/pokemonmart.component.ts
@@ -11,6 +11,7 @@ import { MenuButtonComponent } from '../menu-button/menu-button.component';
   styleUrl: './pokemonmart.component.scss'
 })
 export class PokemonmartComponent {
+  isMenuOpen: boolean = false;
 
   constructor(private router: Router) { }
 
@@ -18,8 +19,45 @@ export class PokemonmartComponent {
 
     if (buildingName === 'Exit') {
       this.router.navigate(['']);
-    } else if (buildingName === 'Pokemon Box') {
-      this.router.navigate(['pokemonbox']);
+    } else if (buildingName === 'Shop') {
+      this.isMenuOpen = !this.isMenuOpen;
     }
   }
+
+  selectOption(option: string) {
+    console.log(`Selected: ${option}`);
+    this.isMenuOpen = false; // Close the menu after selection
+
+    // Handle each option
+    switch (option) {
+      case 'buy':
+        // Navigate to or show the buy item screen
+        this.buyItem();
+        break;
+      case 'sell':
+        // Navigate to or show the sell item screen
+        this.sellItem();
+        break;
+      case 'manage':
+        // Navigate to or show the item management screen
+        this.manageItems();
+        break;
+    }
+  }
+
+  buyItem() {
+    console.log('Opening Buy Items menu...');
+    // Add logic for the buy item feature
+  }
+
+  sellItem() {
+    console.log('Opening Sell Items menu...');
+    // Add logic for the sell item feature
+  }
+
+  manageItems() {
+    console.log('Opening Item Management menu...');
+    // Add logic for the item management feature
+  }
+
 }
